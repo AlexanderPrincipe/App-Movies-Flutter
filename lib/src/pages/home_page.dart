9ddf253ff1f5cd74ad8bf87398a437efcf1faefb
Alexander Principe <principetolentinoa@hotmail.com>
2019-06-07 23:01:04 -0500

    07/06/2019

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,34 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/src/providers/peliculas_providers.dart';
 import 'package:peliculas/src/widgets/card_swiper_widget.dart';
 
 class HomePage extends StatelessWidget {
+
+  final peliculasProvider = new PeliculasProvider();
+
   @override
   Widget build(BuildContext context) {
+
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Peliculas en cines'),
+        centerTitle: false,
+        title: Text('Películas en cines'),
         backgroundColor: Colors.indigoAccent,
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {},
+            icon: Icon( Icons.search ),
+            onPressed: () {
+
+            },
           )
         ],
       ),
       body: Container(
         child: Column(
-          children: <Widget>[_swiperTarjetas()],
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            _swiperTarjetas()
+          ],
         ),
-      ),
+      )
+       
     );
   }
 
   Widget _swiperTarjetas() {
 
-    return CardSwiper(
-      peliculas: [1,2,3,4,5],
+    return FutureBuilder(
+      future: peliculasProvider.getEnCines(),
+      builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+        
+        if ( snapshot.hasData ) {
+          return CardSwiper( peliculas: snapshot.data );
+        } else {
+          return Container(
+            height: 400.0,
+            child: Center(
+              child: CircularProgressIndicator()
+            )
+          );
+        }
+        
+      },
     );
 
 
+
+    
+
+
   }
+
+
 }
--- lib/src/widgets/card_swiper_widget.dart
+++ lib/src/widgets/card_swiper_widget.dart
@@ -1,38 +1,50 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:peliculas/src/models/pelicula_model.dart';
+
 
 class CardSwiper extends StatelessWidget {
-
-  final List<dynamic> peliculas;
-
+  
+  final List<Pelicula> peliculas;
+  
   CardSwiper({ @required this.peliculas });
 
-
+  
   @override
   Widget build(BuildContext context) {
-
-    final _screenSize = MediaQuery.of(context).size;
     
+    final _screenSize = MediaQuery.of(context).size;
 
     return Container(
-      padding: EdgeInsets.only(top: 10.0),
-      width: _screenSize.width * 1,
-      height: _screenSize.height * 0.5,
-      child: Swiper(
-        //layout: SwiperLayout.STACK,
-        itemHeight: 200.0,
-        itemBuilder: (BuildContext context, int index) {
-          return ClipRRect(
-            borderRadius: BorderRadius.circular(20.0),
-            child: Image.network("http://via.placeholder.com/350x150",
-            fit: BoxFit.cover,
-          ),
-          );
-          
-        },
-        itemCount: 3,
-        //pagination: new SwiperPagination(),
-        //control: new SwiperControl(),
+       padding: EdgeInsets.only(top: 10.0),
+       child: Swiper(
+          layout: SwiperLayout.STACK,
+          itemWidth: _screenSize.width * 0.7,
+          itemHeight: _screenSize.height * 0.5,
+          itemBuilder: (BuildContext context, int index){
+
+            peliculas[index].uniqueId = '${ peliculas[index].id }-tarjeta';
+
+            return Hero(
+              tag: peliculas[index].uniqueId,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20.0),
+                child: GestureDetector(
+                  onTap: ()=> Navigator.pushNamed(context, 'detalle', arguments: peliculas[index]),
+                  child: FadeInImage(
+                    image: NetworkImage( peliculas[index].getPosterImg()  ),
+                    placeholder: AssetImage('assets/img/no-image.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                )
+              ),
+            );
+            
+          },
+          itemCount: peliculas.length,
+          // pagination: new SwiperPagination(),
+          // control: new SwiperControl(),
       ),
     );
 
