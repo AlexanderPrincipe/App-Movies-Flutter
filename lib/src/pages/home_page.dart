b66a59356e02c2e4ab375e86d41156e20a2bf171
Alexander Principe <principetolentinoa@hotmail.com>
2019-06-08 22:50:53 -0500

    08/06/2019

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:peliculas/src/providers/peliculas_providers.dart';
 import 'package:peliculas/src/widgets/card_swiper_widget.dart';
+import 'package:peliculas/src/widgets/movie_horizontal.dart';
 
 class HomePage extends StatelessWidget {
 
@@ -28,7 +29,8 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-            _swiperTarjetas()
+            _swiperTarjetas(),
+            _footer(context)
           ],
         ),
       )
@@ -59,6 +61,39 @@ class HomePage extends StatelessWidget {
 
 
     
+
+
+  }
+
+
+
+  Widget _footer(BuildContext context) {
+
+    return Container(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(left: 20.0),
+            child: Text('Populares', style: Theme.of(context).textTheme.subhead)),
+          SizedBox(height: 5.0),
+
+          FutureBuilder(
+            future: peliculasProvider.getPopulares(),
+            builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+
+              if ( snapshot.hasData ) {
+                return MovieHorizontal( peliculas: snapshot.data );
+              }else {
+                return Center(child: CircularProgressIndicator());
+              }
+
+            },
+          ),
+        ],
+      ),
+    );
 
 
   }
--- lib/src/providers/peliculas_providers.dart
+++ lib/src/providers/peliculas_providers.dart
@@ -10,13 +10,7 @@ class PeliculasProvider {
   String _url      = 'api.themoviedb.org';
   String _language = 'es-ES';
 
-
-  Future<List<Pelicula>> getEnCines() async {
-
-    final url = Uri.https(_url, '3/movie/now_playing', {
-      'api_key'  : _apikey,
-      'language' : _language
-    });
+  Future<List<Pelicula>> _procesarRespuesta(Uri url) async {
 
     final resp = await http.get( url );
     final decodedData = json.decode(resp.body);
@@ -25,8 +19,35 @@ class PeliculasProvider {
 
     return peliculas.items;
 
+  }
+
+
+  Future<List<Pelicula>> getEnCines() async {
+
+    final url = Uri.https(_url, '3/movie/now_playing', {
+      'api_key'  : _apikey,
+      'language' : _language
+    });
+
+    return await _procesarRespuesta(url);
+
 
   }
+
+
+  Future<List<Pelicula>> getPopulares() async {
+
+    final url = Uri.https(_url, '3/movie/popular', {
+      'api_key'  : _apikey,
+      'language' : _language
+    });
+
+    return await _procesarRespuesta(url);
+
+
+  }
+
+
 
 
 }
